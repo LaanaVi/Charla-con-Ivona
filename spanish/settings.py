a6e6e61dc3772dde5b5d7e2ada126a0d84fcf154
Lana <lana.vicovac@gmail.com>
2020-09-09 12:52:21 +0200

    debug false

--- spanish/settings.py
+++ spanish/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = config("DEBUG", cast=bool)
+DEBUG = False
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
