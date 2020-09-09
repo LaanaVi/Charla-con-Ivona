438491d0dce51ab17d56c472b34a56f6181aaa07
Lana <lana.vicovac@gmail.com>
2020-09-09 13:19:01 +0200

    DEBUG T F

--- spanish/settings.py
+++ spanish/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
