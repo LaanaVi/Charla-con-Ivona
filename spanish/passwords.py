c694b6069ae5d1c1df88cf18a27e2edbd32233bf
LaanaVi <lana.vicovac@gmail.com>
2020-10-26 16:43:57 +0100

    Delete passwords.py

--- spanish/passwords.py
+++
@@ -1,9 +0,0 @@
-SECRET_KEY=b8r@xnqjxxs#8p8h9083*l6^$o*)n5#nn55qyv&1gdq6wbrw_-
-NAME='charla_con_ivona'
-USER='Ivona'
-PASSWORD='Apokalipsa90'
-HOST='charla-con-ivona.cfrqsmromv6q.us-west-2.rds.amazonaws.com'
-MAIL_HOST_USER='charlaconivona@gmail.com'
-EMAIL_HOST_PASSWORD='mdwfkgkqxxldupcq'
-DEBUG=False
-
